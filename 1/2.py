22a58968160ae17b0e9b5edee6cbdb3bbaba2ef9
AleX <alexgibbs606@gmail.com>
2022-12-01 10:57:33 -0600

    Completed first exercise

---
+++ 1/2.py
@@ -0,0 +1,12 @@
+#!/usr/env python310
+
+import sys
+from pathlib import Path
+
+# Getting our input file path
+inFileName = Path() / '1' / 'example.txt' if len(sys.argv) != 2 else sys.argv[1]
+
+with open(inFileName, 'r', encoding='UTF-8') as inFile:
+	elfCals = [sum([int(cal) for cal in elf.split('\n')]) for elf in inFile.read().strip().split('\n\n')]
+	elfCals.sort()
+	print(sum(elfCals[-3:]))
